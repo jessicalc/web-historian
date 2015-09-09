7f984f89ed49db8cccd9d74f44ee00d8497649aa
jessicalc <jessicalaurenchong@gmail.com>
2015-09-09 14:20:38 -0700

    Handling basic get request and built action object for different request methods.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
 // require more modules/folders here!
 
+//Handling different http request methods
+var actions = {
+  'OPTIONS': function(req, res) {
+
+  },
+  'GET': function(req, res) {
+    helpers.serveAssets(res, archive.paths.siteAssets + '/index.html', function(data) {
+      console.log('our data is', data);
+      res.writeHead(200, helpers.headers);
+      res.end(data);
+    })
+  },
+  'POST': function(req, res) {
+
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  actions[req.method](req,res);
+  // res.end(archive.paths.list);
 };
